ba5de04079e998459c0e2463a4406249e6d5b246
Canberk <renticar@RentiCar-MacBook-Pro.local>
2023-03-22 17:08:41 +0300

    header improvements

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,72 +1,207 @@
+import { useState } from "react";
 import styles from "./Header.module.css";
 import PersonIcon from "@mui/icons-material/Person";
-import ExploreIcon from "@mui/icons-material/Explore";
+import SearchIcon from "@mui/icons-material/Search";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
 import GradeIcon from "@mui/icons-material/Grade";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function Header() {
+  const [burgerMenu, setBurgerMenu] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleOpenBurgerMenu = (event) => {
+    setBurgerMenu(event.currentTarget);
+  };
+
+  const handleCloseBurgerMenu = () => {
+    setBurgerMenu(null);
+  };
+
+  const settings = ["Giriş Yap", "Kayıt Ol"];
+
   return (
-    <>
-      <main className={styles.main}>
-        <div>
-          <nav className={styles.header}>
-            <li style={{ marginTop: "-1vw" }}>
-              <a href="/">
-                <img
-                  style={{ height: "43px", marginTop: "5px" }}
-                  src="http://192.168.1.102:8000/images/siyah4.png"
-                />
-              </a>
-            </li>
-
-            <li>
-              <a href="/son-icerikler">
-                <AccessTimeIcon />
-                Son İçerikler
-              </a>
-            </li>
-            <li style={{ marginTop: "5px" }}>
-              <a href="/trendler">
-                <TrendingUpIcon />
-                Trendler
-              </a>
-            </li>
-            <li style={{ marginTop: "5px" }}>
-              <a href="/sicak-icerikler">
-                <WhatshotIcon />
-                Sıcak İçerikler
-              </a>
-            </li>
-            <li style={{ marginTop: "5px" }}>
-              <a href="/populer-icerikler">
-                <GradeIcon />
-                Popüler
-              </a>
-            </li>
-
-            <li
-              style={{
-                marginTop: "1.5vw",
-                float: "right",
-                cursor: "pointer",
-                marginRight: "3vw",
+    <AppBar position="static" className={styles.header}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Tooltip title="Open settings">
+            <IconButton
+              onClick={handleOpenBurgerMenu}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                color: "#1A1A1A",
+                fontWeight: "bold",
               }}
             >
-              <PersonIcon size="large" />
-            </li>
+              <MenuIcon />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="burger-menu-appbar"
+            anchorEl={burgerMenu}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(burgerMenu)}
+            onClose={handleCloseBurgerMenu}
+          >
+            <Box sx={{ width: "30vw", display: { xs: "none", md: "flex" } }}>
+              <Typography textAlign="center">Kategoriler</Typography>
 
-            <li
-              style={{ marginTop: "5px", float: "right", marginRight: "5px" }}
+              <MenuItem>
+                <Typography textAlign="center">Kategoriler</Typography>
+              </MenuItem>
+            </Box>
+          </Menu>
+
+          <AdbIcon />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, ml: 4, display: { xs: "none", md: "flex" } }}>
+            <Button
+              sx={{
+                color: "#1A1A1A",
+                fontWeight: "bold",
+                ml: 2,
+              }}
+              startIcon={<AccessTimeIcon />}
             >
-              <a href="/kesfet">
-                <ExploreIcon size="large" />
-              </a>
-            </li>
-          </nav>
-        </div>
-      </main>
-    </>
+              Son İçerikler
+            </Button>
+
+            <Button
+              sx={{
+                color: "#1A1A1A",
+                fontWeight: "bold",
+                ml: 2,
+              }}
+              startIcon={<TrendingUpIcon />}
+            >
+              Trendler
+            </Button>
+
+            <Button
+              sx={{
+                color: "#1A1A1A",
+                fontWeight: "bold",
+                ml: 2,
+              }}
+              startIcon={<WhatshotIcon />}
+            >
+              Sıcak İçerikler
+            </Button>
+
+            <Button
+              sx={{
+                color: "#1A1A1A",
+                fontWeight: "bold",
+                ml: 2,
+              }}
+              startIcon={<GradeIcon />}
+            >
+              Popüler
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                color: "#1A1A1A",
+                fontWeight: "bold",
+              }}
+            >
+              <SearchIcon />
+            </IconButton>
+            <Tooltip title="Open settings">
+              <IconButton
+                onClick={handleOpenUserMenu}
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{
+                  color: "#1A1A1A",
+                  fontWeight: "bold",
+                }}
+              >
+                <PersonIcon />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
