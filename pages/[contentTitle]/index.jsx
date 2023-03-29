27f000655c97e866563818ffab7f171f3140f19b
Canberk <highpriv@gmail.com>
2023-03-29 17:45:11 +0300

    title & summary & info field created -- image bgcolor & change UI

--- pages/[contentTitle]/index.jsx
+++ pages/[contentTitle]/index.jsx
@@ -12,6 +12,50 @@ export default function singlePage() {
           width={1200}
           height={600}
         />
+
+        <div className={styles.contentTitle}>
+          <Components.Typography
+            variant="h2"
+            color="#dcdcdc"
+            sx={{ fontWeight: "bold" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </Components.Typography>
+
+          <Components.Typography variant="body2" color="#dcdcdc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            interdum porttitor est eu luctus. Nullam sagittis volutpat dui in
+            venenatis. Sed eget tortor non arcu auctor facilisis. Donec tempor
+            mauris odio, vitae pellentesque purus feugiat ac. Donec aliquam,
+            nisi id consequat elementum, orci eros viverra lorem, quis gravida
+            dolor leo sit amet est. Cras pretium, tellus vitae finibus finibus,
+            tellus ligula venenatis ligula, a fringilla mauris augue ut augue.
+            Curabitur fringilla justo quis massa congue tempus. Aliquam tortor
+            lacus, facilisis ut eros quis, sodales commodo enim. Praesent libero
+            urna, vehicula a tincidunt non, malesuada in felis. Mauris at
+            ultrices arcu. Aliquam erat volutpat. Suspendisse ultricies, purus
+            eget facilisis feugiat, dolor nisl condimentum ipsum, sit amet
+            tincidunt metus mi et tellus. Pellentesque rhoncus lacinia
+            tincidunt.
+          </Components.Typography>
+
+          <div className={styles.contentInfo}>
+            <Components.Typography
+              variant="subtitle1"
+              color="#dcdcdc"
+              sx={{ fontWeight: "bold" }}
+            >
+              <b>Paylaşan:</b> @canberkberen1
+            </Components.Typography>
+            <Components.Typography
+              variant="subtitle1"
+              color="#dcdcdc"
+              sx={{ fontWeight: "bold" }}
+            >
+              <b>Yayınlanma Tarihi:</b> 22.03.2023 10:30
+            </Components.Typography>
+          </div>
+        </div>
       </div>
 
       <Components.Grid
